84ca430c8499f0e3427186ba9b1c2119122be0d7
Phạm Khải Hoàn <92038544+Hoanpham1@users.noreply.github.com>
2021-12-12 12:47:40 +0700

    demo

---
+++ menu.php
@@ -0,0 +1,16 @@
+<div class="menu">
+	<div class="logo">
+		<a href="index.php">
+			S2STORE
+		</a>
+	</div>
+	<div class="menu-right">
+		<div class="search-container">
+			<form action="index.php">
+				<input type="text" placeholder="Search..." name="search">
+				<button type="submit"><i class="fa fa-search"></i></button>
+			</form>
+		</div>
+		<a href="#">ĐĂNG NHẬP</a>
+	</div>
+</div>
---
+++ sidebar.php
@@ -0,0 +1,49 @@
+<div class="container">
+    <div class="navigation">
+        <ul>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                    </span>
+                    <span class="title">Chung</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="person-outline"></ion-icon>
+                    </span>
+                    <span class="title">Người dùng</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="cart-outline"></ion-icon>
+                    </span>
+                    <span class="title">Sản phẩm</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                    </span>
+                    <span class="title">Tin nhắn</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="settings-outline"></ion-icon>
+                    </span>
+                    <span class="title">Cài đặt</span>
+                </a>
+            </li>
+
+    </div>
+</div>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
